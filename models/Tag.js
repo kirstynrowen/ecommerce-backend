ec30bbc2d83e0704832e82f1133a15716a7672a1
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-06-24 16:55:11 -0700

    completed product, producttag, and category models

--- models/Tag.js
+++ models/Tag.js
@@ -11,7 +11,7 @@ Tag.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: truw,
+      autoIncrement: true,
     },
     tag_name: {
       type: DataTypes.STRING,
