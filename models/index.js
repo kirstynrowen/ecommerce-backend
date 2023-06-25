47bc14471faf16ec746cfb64d3e9a933fc7f30fc
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-06-24 17:18:13 -0700

    tag and product relationships

--- models/index.js
+++ models/index.js
@@ -14,8 +14,21 @@ Category.hasMany(Product, {
   onDelete: 'CASCADE',
 })
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'product_tags',
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'tag_products',
+})
 
 module.exports = {
   Product,
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
     res.status(200).json(tagData);
   } catch (error) {
     res.status(500).json(error);
-    console.log(error)
+    console.log(error);
   }
 });
 
