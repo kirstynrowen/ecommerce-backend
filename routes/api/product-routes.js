b4f625a3727085571d88b15d4dbdfd480b6bf64b
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-06-24 17:26:35 -0700

    testing getproducts route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [
+        { 
+          model: Category, 
+        },
+        {
+          model: Tag,
+          through: ProductTag,
+          attributes: [],
+          as: 'product_tags',
+        },
+      ],
+    });
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+    console.log(error);
+  }
+
 });
 
 // get one product
